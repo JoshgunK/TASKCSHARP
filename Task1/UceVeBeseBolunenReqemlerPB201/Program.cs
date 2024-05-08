7a412f3b443e461f571ceeb5d15f5f4081ba1312
Wheaxy Sueido <joshgunvk@code.edu.az>
2024-05-08 22:43:52 +0400

    Tapsiriqlar muveffeqiyyetle yerine yetirildi

---
+++ Task1/UceVeBeseBolunenReqemlerPB201/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace UceVeBeseBolunenReqemlerPB201
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int number = 1; number <=100; number++)
+            {
+                if (number % 3 == 0 && number % 5 == 0)
+                {
+                    Console.WriteLine($"{number} Uce Ve Bese Bolune Biler.");
+                }
+            }
+        }
+    }
+}
---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Task2
+{
+    internal class Task2
+    {
+        static void Main(string[] args)
+        {
+            int Bal = 85;
+            if (Bal < 65)
+            {
+                Console.WriteLine("Kesildiniz");
+            }
+            else if (Bal >= 65 && Bal < 85)
+            {
+                Console.WriteLine("Tebrikler Adi Diploma Layiq Goruldunuz");
+            }
+            else if (Bal >= 85 && Bal < 95)
+            {
+                Console.WriteLine("Tebrikler Seref Diplomuna Layiq Goruldunuz!");
+            }
+            else if (Bal >= 95 && Bal <= 100)
+            {
+                Console.WriteLine("***TEBRIKLER ALI SEREF DIPLOMUNA LAYIQ GORULDUNUZ!***");
+            }
+            else if (Bal > 100)
+            {
+                Console.WriteLine("Tebrikler Easter Egg Tapdiniz :) PB201");
+            }
+
+        }
+    }
+}
